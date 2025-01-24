9cf0b2dd06a8ec6ddbd2bc2dabe88a5c36aad5b8
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2025-01-24 14:12:17 +1100

    Tweak file validator to detect file type, circumvent htsfile for htsget endpoints since file determination is not implemented on samtools (yet)

--- ga4gh/htsget/compliance/file_validator.py
+++ ga4gh/htsget/compliance/file_validator.py
@@ -1,4 +1,6 @@
 import os
+import requests
+from ga4gh.htsget.compliance.config import constants as c
 
 class FileValidator(object):
 
@@ -8,7 +10,22 @@ class FileValidator(object):
     def __init__(self, returned_fp, expected_fp):
         self.set_returned_fp(returned_fp)
         self.set_expected_fp(expected_fp)
-    
+
+    def identify_file_type(self, fp, source):
+        '''
+        source = from htsget or from (local) filesystem
+        '''
+
+        if "local_fs" in source:
+            return os.popen("htsfile " + fp)
+
+        # Samtools' hstfile does not (yet) support (detailed?) htsget file identification ¯\_(ツ)_/¯
+        #
+        # % htsfile https://htsget.ga4gh-demo.org/reads/htsnexus_test_NA12878
+        # https://htsget.ga4gh-demo.org/reads/htsnexus_test_NA12878:      htsget text
+        elif "htsget" in source:
+            return requests.get(fp).json()["htsget"]["format"]
+
     def validate(self):
 
         result = FileValidator.SUCCESS
@@ -22,22 +39,29 @@ class FileValidator(object):
         return result
 
     def load(self, fp):
-        file_type = os.popen("htsfile " + fp)
+        file_type = ""
         ext = ""
-        s = ""
+        samtools_string = ""
 
-        if 'BAM' in file_type:
-            ext = ".bam"
-        elif 'CRAM' in file_type:
-            ext = ".cram"
-        elif 'VCF' in file_type:
-            ext = ".vcf.gz"
-        elif 'BCF' in file_type:
-            ext = "bcf"
+        if "http" in fp:
+            file_type = self.identify_file_type(fp, "htsget")
         else:
-            s = self.load_binary(fp+ext)
+            file_type = self.identify_file_type(fp, "local_fs")
 
-        return s
+
+        if c.FORMAT_BAM in file_type:
+            ext = c.EXTENSION_BAM
+        elif c.FORMAT_CRAM in file_type:
+            ext = c.EXTENSION_CRAM
+        elif c.FORMAT_VCF in file_type:
+            ext = c.EXTENSION_VCF
+        elif c.FORMAT_BCF in file_type:
+            ext = c.EXTENSION_BCF
+        else:
+            samtools_string = self.load_binary(fp+ext)
+
+        return samtools_string
+
 
     def load_binary(self, fp):
         s = []
